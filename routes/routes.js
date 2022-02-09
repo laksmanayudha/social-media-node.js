000c93b6537f6a0d78550403cde43473a2904a1d
laksmanayudha <laksmanayudha22@gmail.com>
2022-02-09 17:55:02 +0800

    add signup view

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,12 @@ Router.get("/", (req, res) => {
     res.render('login')
 })
 
+Router.get("/login", (req, res) => {
+    res.render('login')
+})
+
+Router.get("/signup", (req, res) => {
+    res.render('signup')
+})
+
 module.exports = Router;
